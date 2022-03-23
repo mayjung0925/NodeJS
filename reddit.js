c989822d8e4a63d15114f1fd7d07e2e4c6dc997b
Eunjin Jeoung <mayjung0925@gmail.com>
2022-03-23 16:40:08 -0500

    added lines to explain the code

--- reddit.js
+++ reddit.js
@@ -4,20 +4,22 @@ const request = require('request');
 
 let dataPath = path.join(__dirname, './popular-articles.json')
 
-
+// request data from the following website
 request('https://reddit.com/r/popular.json', (err, res, body) => {
     if (err) console.log(err);
 
     let dataArray = []
 
+//convert body data into JS object and loop through each item to extract the following data; title, author, url
     JSON.parse(body).data.children.forEach(item => {
         obj = {
             title: item.data.title,
             author: item.data.author,
             url: item.data.url
         }
-        dataArray.push(obj);
+        dataArray.push(obj);// push each obj into dataArray which is in the request scope
 
+//convert dataArray(JS) into JSON and write in the desiginated file 
         fs.writeFile(dataPath, JSON.stringify(dataArray), (err) => {
             if (err) console.log(err);
         })
